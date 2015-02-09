b58d27cf7eec33efc7df8ad4dd579d10484a5b7e
Feng Ling <fling@utexas.edu>
2015-02-09 03:57:59 -0600

    new commit

--- simulation/graphs.m
+++ simulation/graphs.m
@@ -1,10 +1,10 @@
 figure();hold all;
-plot([0 .47],[0 .62],'kx:','markersize',10);
+plot([0 .47],[0 .62],'k:','markersize',10);
 plot([.47 1],[.62 0],'k:');
 % rint
 plot([0 1],[0 0],'kx-','markersize',10);
 % rtil
-plot([0 .3],[0 0],'r--','linewidth',2);
+plot([0 .3],[0 0],'b--','linewidth',2);
 text(.15,-.1,'$\tilde{r}$','interpreter','latex');
 % Rtil
 plot([.3 .47],[0 .62],'b-','linewidth',2);
@@ -12,6 +12,10 @@ text(.42,.34,'$\tilde{R}$','interpreter','latex');
 % theta
 ang([.3 0],.08,[0 1.2],'k');
 text(.37,.08,'$\theta$','interpreter','latex');
+% incident site
+plot(0.3,0,'r*','linewidth',2,'markersize',12);
+% crash site
+plot(.47,.62,'k*','linewidth',2,'markersize',12);
 
 xlim([-1 2]); ylim([-1 1]);
 set(gca,'visible','off');
